a0758aff8b4039d5e309a6128143ec63bca45266
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-10-23 15:26:04 +0530

    Project SetUp ✅

---
+++ src/MarkleAirdrop.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract MarkleAirdrop {
+    // list of addresses that can receive tokens
+    // allow someone in the list to claim some ERC20 tokens
+
+    address[] claimers;
+
+    function claim(address account) external {
+        // Markle Proofs
+    }
+}
