9fc69112fb38bdddb9428b3665bd3a99f67563fc
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-11-05 13:08:23 +0530

    Create Claiming Script

---
+++ script/Interactions.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import { Scrip } from "forge-std/Scrip.sol";
+import { DevOpsTools } from "foundry-devops/src/DevOpsTools.sol";
+import { MerkleAirdrop } from "../src/MerkleAirdrop.sol";
+
+contract ClaimAirdrop is Script {
+    address CLAIMING_ADDRESS = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    uint256 CLAIMING_AMOUNT = 25 * 1e18;
+    bytes32 PROOF_ONE = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
+    bytes32 PROOF_TWO = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] PROOF = [PROOF_ONE, PROOF_TWO];
+
+    function claimAirdrop(address airdrop) public {
+        vm.startBroadcast();
+        MerkleAirdrop(airdrop).claimTokens(CLAIMING_ADDRESS, CLAIMING_AMOUNT, PROOF, v, r, s);
+        vm.endBroadcast();
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("MerkleAirdrop", block.chainid);
+        claimAirdrop(mostRecentlyDeployed);
+    }
+}
