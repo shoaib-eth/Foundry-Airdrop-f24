9a7d56253c75b28f055691cba50d33cc164e6ee9
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-11-05 14:08:10 +0530

    Splitting a Signature

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -11,11 +11,27 @@ contract ClaimAirdrop is Script {
     bytes32 PROOF_ONE = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
     bytes32 PROOF_TWO = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
     bytes32[] PROOF = [PROOF_ONE, PROOF_TWO];
+    bytes private SIGNATURE =
+        hex"6c576a2bcd6ec720be706c4a336ac69c3cfee293689bca8205e9ff240e2d69c50084e9509ae54535c86062b05a91e5da8bb22d517e7f72ba879823fd3f56c9361b";
+
+    error __ClaimAirdropScript_InvalidSignatureLength();
 
     function claimAirdrop(address airdrop) public {
         vm.startBroadcast();
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(SIGNATURE);
         MerkleAirdrop(airdrop).claimTokens(CLAIMING_ADDRESS, CLAIMING_AMOUNT, PROOF, v, r, s);
         vm.endBroadcast();
+    }
+
+    function splitSignature(bytes memory sig) public pure returns (uint8 v, bytes32 r, bytes32 s) {
+        if (sig.length != 65) {
+            revert __ClaimAirdropScript_InvalidSignatureLength();
+        }
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
     }
 
     function run() external {
